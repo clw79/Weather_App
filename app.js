9e70392cd7983ed0c657d328d98fec337bf82b7c
Chris Williams <cwdevportfolio@gmail.com>
2024-07-25 02:18:53 -0500

    added selectors for input and button

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const apiKey = "c7ff465c45bf9b9d3d7c7bb85b19043a";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=lehi";
 
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+
 //GETTING INFORMATION FROM API
 async function checkWeather() {
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
