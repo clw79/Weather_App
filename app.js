06dc63c4e8aab847cb6052be63b674f2ebbc7469
Chris Williams <cwdevportfolio@gmail.com>
2025-02-28 14:17:35 -0700

    finish

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 //API CONNECTION
 const apiKey = "c7ff465c45bf9b9d3d7c7bb85b19043a";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=lehi";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=";
 
-const searchBox = document.querySelector(".search input")
-const searchBtn = document.querySelector(".search button")
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value)
+    console.log("button clicked");
+});
+
 
 //GETTING INFORMATION FROM API
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
-
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await response.json();
 
     //DISPLAYING API INFORMATION ON APP
@@ -17,11 +24,26 @@ async function checkWeather() {
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "mph";
 
-    searchBtn.addEventListener("click", () => {
-        checkWeather(searchBox.value)
-    });
+    switch (data.weather[0].main) {
+        case "Clouds":
+            weatherIcon.src = "images/clouds.png";
+            break;
+        case "Snow":
+            weatherIcon.src = "images/snow.png";
+            break;
+        case "Drizzle":
+            weatherIcon.src = "images/drizzle.png";
+            break;
+        case "Mist":
+            weatherIcon.src = "images/mist.png";
+            break;
+        case "Rain":
+            weatherIcon.src = "images/rain.png";
+            break;
+        default:
+            weatherIcon.src = "images/clear.png";
+            break;
 
-    console.log(data);
+    }
+
 }
-
-checkWeather();
